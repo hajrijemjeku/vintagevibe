1feaedcd6eddff9a530ef4ea6a3b30067e56ddb1
hajrijemjeku <hajrijemj@gmail.com>
2024-09-10 16:07:13 +0200

    Similar products and show all product images

--- product_details.php
+++ product_details.php
@@ -43,7 +43,59 @@
                     <p>Size:<?php echo $productdetails['size']; ?></p>
 
                 </div>
+                <div class="p-3 border mt-3">
+                    <h4>Product Info</h4>
+                    <p>Name:<?php echo $productdetails['name']; ?></p>
+                    <p>Price:<?php echo $productdetails['price']; ?> &euro;</p>
+                    <p>Size:<?php echo $productdetails['size']; ?></p>
+
+                </div>
+
             </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-12">
+                <hr class="mx-auto mt-5 border-secondary">
+            </div>
+           
+        </div>
+
+        <div class="similar-products">
+            <h3 class=" text-secondary">Similar products</h3>
+            <div class="row mt-4">
+                <?php
+                    $similarproducts = $crudObj->search('product',['id','name','price','size'],['name'=>$productdetails['name']],'');
+                    $similarproducts = $similarproducts->fetchAll();
+                    
+                    foreach($similarproducts as $similarproduct):
+                        $images = $crudObj->select('image',['id','src','alt','productid'],['productid'=> $similarproduct['id']],'','');
+                        $images = $images->fetchAll();
+                        if(!($similarproduct['id']== $productdetails['id'])):
+                ?>
+                <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                    <a href="product_details.php?product_id=<?=$similarproduct['id'];?>" class="text-decoration-none">
+                        <div class="card" style="width: 18rem;">
+                            <input type="hidden" name="product_id" id="product_id" value="<?= $similarproduct['id'] ?>">
+                            <img src="./assets/images/products/<?= $images[0]['src']; ?>" class="card-img-top" alt="<?= $images[0]['alt']; ?>" height="300px">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $similarproduct['name'] ?></h5>
+                                <p class="card-text"> 
+                                    Price: <strong> <?php echo number_format($similarproduct['price'],2);  ?> &euro; </strong> / Size: <strong><?= $similarproduct['size'] ?> </strong>
+                                </p>
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+
+                <?php
+                    else: continue; endif; endforeach;
+                ?>
+
+            </div>
+
         </div>
     </div>
     <!-- <script>
