4a2671a73545d6cbeb89fc2179b641fecdbcf973
hajrijemjeku <hajrijemj@gmail.com>
2024-09-17 02:38:02 +0200

    all products added

--- products.php
+++ products.php
@@ -5,7 +5,20 @@
 
 
 ?>
-
+<style>
+    .out-of-stock-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        z-index: 10; /* Ensure it appears above other content */
+        opacity: 0.5; /* Adjust opacity if needed */
+    }
+</style>
 <section class="products py-5">
     <div class="container" style="background-color:lightblue">
         <h2 class="text-center">Products.php</h2>
@@ -42,9 +55,6 @@
         <div class="row mt-4">
 
         <?php
-            // $crudObj = new Crud($pdo);
-            //$products = $crudObj->select('product',['id','name','price', 'size'],[] ,'', 'DESC');
-            //   print_r($products->fetch());
 
             $eraId = isset($_GET['era_id']) ? intval($_GET['era_id']) : 0;
             $categoryId = isset($_GET['category_id']) ? intval($_GET['category_id']) : 0;
@@ -74,15 +84,20 @@
                 if ($eraId) {
                     //$eraId = isset($_GET['era_id']);
                     $crudObj = new Crud($pdo);
-                    $products = $crudObj->search('product', ['id', 'name', 'price','size'], ['eraid' => $eraId],'');
+                    $products = $crudObj->search('product', ['id', 'name', 'price','size', 'qty'], ['eraid' => $eraId],'');
                     $products = $products->fetchAll();
                 }
                 if($categoryId) {
                     //$eraId = isset($_GET['era_id']);
                     $crudObj = new Crud($pdo);
-                    $products = $crudObj->search('product', ['id', 'name', 'price','size'], ['categoryid' => $categoryId],'');
+                    $products = $crudObj->search('product', ['id', 'name', 'price','size','qty'], ['categoryid' => $categoryId],'');
                     $products = $products->fetchAll();
                 } 
+                if(!$eraId && !$categoryId){
+                    $crudObj = new Crud($pdo);
+                    $products = $crudObj->select('product', ['id', 'name', 'price','size','qty'], [],'','');
+                    $products = $products->fetchAll();
+                }
             }
             
             
@@ -103,6 +118,7 @@
                 // $base_url = 'http://localhost/php/yv/vintagevibe/products.php?category_id=';
                 // $endurl = $_GET['category_id'];
                 // $url = $base_url . $endurl;
+                if($product['qty']>0): 
                     
         ?>
                 <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
@@ -119,25 +135,31 @@
                                 Price: <strong> <?php echo number_format($product['price'],2);  ?> &euro; </strong> / Size: <strong><?= $product['size'] ?> </strong>
                             </p>
 
-
-                            <!-- <?//php if (in_array($product['id'], $wishlistItems)): ?>
-                                <form method="post" style="display:inline;">
-                                    <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
-                                    <input type="hidden" name="action" value="remove">
-                                    <button class="btn-wishlist"><i class="fa-solid fa-heart"></i></button>
-                                </form>
-                            <?//php else: ?>
-                                <form method="post" style="display:inline;">
-                                    <input type="hidden" name="product_id" value="<?=$product['id'] ?>">
-                                    <input type="hidden" name="action" value="add">
-                                    <button class="btn-wishlist"><i class="fa-regular fa-heart"></i></button>
-                                </form>
-                            <?//php endif; ?> -->
                         </div>
                     </div>
                 </div>
+                <?php else: ?>
+                    <div class="col-lg-3 col-md-3 col-sm-12 mb-3">
+                    <!-- <a href="product_details.php?product_id=<?//=$product['id'];?>" class="text-decoration-none"> -->
+                        <div class="card" style="width: 18rem;">
+                        <img src="./assets/images/products/out-of-stock.jpg" class="out-of-stock-overlay" alt="Out of Stock">
+                            <input type="hidden" name="product_id" id="product_id" value="<?= $product['id'] ?>">
+                            <img src="./assets/images/products/<?= $image[0]['src']; ?>" class="card-img-top" alt="<?= $image[0]['alt']; ?>" height="300px">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $product['name'] ?></h5>
+                                <p class="card-text"> 
+                                    Price: <strong> <?php echo number_format($product['price'],2);  ?> &euro; </strong> / Size: <strong><?= $product['size'] ?> </strong>
+                                </p>
 
-            <?php endforeach; }; ?>
+                            </div>
+                        </div>
+                    <!-- </a> -->
+                    </div>
+
+
+
+
+            <?php endif; endforeach; }; ?>
 
         </div>
     </div>
