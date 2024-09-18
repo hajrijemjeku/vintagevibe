1e41c6f619703666234bfaac56db5d33cf950977
hajrijemjeku <hajrijemj@gmail.com>
2024-09-18 02:30:56 +0200

    gui improvements

--- dashboard.php
+++ dashboard.php
@@ -11,26 +11,31 @@ include('includes/header.php');  ?>
 
 
     <?php if(!(isset($_SESSION['is_user']) && $_SESSION['is_user'] == true)): ?>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
+        <div class="row text-center">
+            <h2 class="text-center mt-5 mb-5" style="color:darkolivegreen;"> Dashboard </h2>
+
+            <div class="col-md-4 mb-3">
+                <div class="card shadow-sm border-light">
                     <div class="card-body">
-                        <a class="text-decoration-none fs-5 link-secondary" href ="manage-products.php">Manage Products</a>
+                        <h5 class="card-title mb-3">Manage Products</h5>
+                        <a class="btn btn-success" href ="manage-products.php">Manage Products</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card">
+            <div class="col-md-4 mb-3">
+                <div class="card shadow-sm border-light">
                     <div class="card-body">
-                        <a class="text-decoration-none fs-5 link-secondary" href="manage-orders.php">Manage Orders</a>
+                        <h5 class="card-title mb-3">Manage Orders</h5>
+                        <a class="btn btn-success" href="manage-orders.php">Manage Orders</a>
                     </div>
                 </div>
             </div>
             <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == true): ?>
-                <div class="col-md-3">
-                    <div class="card">
+                <div class="col-md-4 mb-3">
+                    <div class="card shadow-sm border-light">
                         <div class="card-body">
-                            <a class="text-decoration-none fs-5 link-secondary" href="manage-users.php">Manage Users</a>
+                            <h5 class="card-title mb-3">Manage Users</h5>
+                            <a class="btn btn-success" href="manage-users.php">Manage Users</a>
                         </div>
                     </div>
                 </div>
