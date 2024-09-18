f54c38bb22644aebfec22098561f80826dbba7a8
hajrijemjeku <hajrijemj@gmail.com>
2024-09-18 02:42:03 +0200

    my-orders.php, final commit

--- my-orders.php
+++ my-orders.php
@@ -33,6 +33,7 @@ if(isset($_SESSION["logged_in"]) && ($_SESSION["logged_in"] == true)) {
                 <?php foreach($myorders as $order): 
                     
                 ?>
+                <p style="color:goldenrod"></p>
                 <tr>
                     <td><?= $order['id'] ?></td>
                     <td><?= $order['fullname'] ?></td>
@@ -41,11 +42,15 @@ if(isset($_SESSION["logged_in"]) && ($_SESSION["logged_in"] == true)) {
                     <td><?= $order['notes'] ?></td>
                     <td><?= $order['total'] ?></td>
                     <td><?= $order['created_at'] ?></td>
-                    <td><?= $order['send_status'] ?></td>
+                    <td style="<?php if($order['send_status'] == 'pending') {echo 'color:goldenrod';} else if($order['send_status'] == 'sent') {echo 'color:green';} else {echo 'color:red';} ?>"><?= $order['send_status'] ?></td>
                 </tr>
                 <?php endforeach; ?>
             </table>
-            <?php else: echo '<p>0 Orders </p>'; ?>
+            <?php else: ?>
+                <h2 class="text-center mt-5" style="color:darkolivegreen;"> You've got (<?= count($myorders); ?>) Orders</h2>
+                <p class="text-center mt-5" style="color:darkslategrey;">You haven’t made any orders yet. 
+                    <a  href="products.php" style="color:#00d974;"  class="link rounded text-decoration-none ">Explore our products</a> and make your first purchase today!
+                </p>
         </div>
         <?php endif; ?>
 
