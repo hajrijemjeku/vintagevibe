3e2202bfba3bbbf6ae3255703b6dec1dfad40b49
hajrijemjeku <hajrijemj@gmail.com>
2024-09-18 02:39:19 +0200

    signin.php, final commit

--- signin.php
+++ signin.php
@@ -8,18 +8,19 @@ $errors = [];
 
         $email = $_POST['email'];
         $password = $_POST['password'];
-        // $remember = $_POST['remember'];
-        //$remember = isset($_POST['remember']) ? $_POST['remember'] : null;
 
         if(!empty($email) && !empty($password)){
 
             if(filter_var($email, FILTER_VALIDATE_EMAIL)){
                 $user = (new Crud($pdo))->select('person',[],['email'=>$email], 1, '');
-                $user = $user -> fetch(); //i kthen te dhanat
-                //echo "<pre>";
-                //print_r($user);
+                $user = $user -> fetch(); 
+
+                if(!$user){
+                    $errors[] = 'Not registered! Go to SignUp to create an account.';
+
+                }else{
                 if(password_verify($password, $user['password'])){
-                    //echo "test";
+
                     $_SESSION['logged_in'] = true;
                     $_SESSION['email'] = $email;
                     $_SESSION['user_id'] = $user['id'];
@@ -33,20 +34,10 @@ $errors = [];
                     }
                     
 
-
-                    // if($remember == 1){
-                    //     $expires = time() + (10*24*60*60);
-                    //     setcookie('remember', true, $expires);
-                    //     setcookie('user_id', $user['id'], $expires);
-                    //     setcookie('email', $email, $expires);
-                    //     setcookie('is_admin', $user['is_admin'], $expires);
-
-                    // }
-
                    header('Location:account.php');            
                 }else{
                     $errors[] = 'Wrong credentials!';
-                }
+                }}
 
             }else{
                 $errors[] = 'Enter a valid email address';
@@ -56,15 +47,14 @@ $errors = [];
         else{
             $errors[] = 'username or password empty';
         }
-
     }
 
 ?>
 
-<section class="login my-5">
-    <div class="container  d-flex justify-content-center">
-        <div class="login-form w-50">
-            <h3 class="mb-3 text-center">Login</h3>
+<section class="signin my-5">
+    <div class="container  d-flex justify-content-center" >
+        <div class="signin-form p-4 w-50 rounded" style="background-color:rgba(116, 148, 100, 0.1)">
+            <h3 class="mb-3 text-center" style="font-family: Arial, sans-serif; font-weight: bold; color: #7b9b77;">Sign In</h3>
             <?php if(count($errors)>0): ?>
             <div class="alert alert-warning">
                 <?php foreach($errors as $error): ?>
@@ -77,7 +67,7 @@ $errors = [];
             <div class="alert alert-info">
                 Registered successfully.
                 <br>
-                Please log in!
+                Please Sign In!
             </div>
             <?php endif; ?>
             <form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
@@ -90,16 +80,12 @@ $errors = [];
                     <label for="password" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control"  id="password">
                 </div>
-                <!-- <div class="mb-3 form-check">
-                    <input type="checkbox" name="remember" class="form-check-input" id="remember" value="1">
-                    <label for="remember" class="form-check-label">Remember me</label>
-                </div> -->
                 
-            <button type="submit" name="signin-btn" class="btn btn-primary">Sign In</button>
+            <button type="submit" name="signin-btn" class="btn btn-success">Sign In</button>
            
             </form>
             <div class="mt-4">
-                <a href="signup.php">Don't have an account? Register now!</a>
+                <a href="signup.php" style="color:#00d974; float: right;"  class="link rounded text-decoration-none ">Don't have an account? Register now!</a>
             </div>
         </div>
     </div>
